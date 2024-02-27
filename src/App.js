6776761aadc6b8b5ec7382e7672070a34b68ea5c
julia Wager <julia@p-wagner.at>
2024-02-27 21:52:46 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import styles from "./App.module.css"
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import {Route,Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div classNam={styles.App}>
+    <div className={styles.App}>
       <NavBar />
+        <Container className={styles.Main}>
+            <Routes>
+                <Route path="/" element={<h1>Home page</h1>} />
+                <Route path="/signin" element={<h1>Sign in</h1>} />
+            </Routes>
+        </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,23 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "../assets/logo.png"
 import styles from "../styles/NavBar.module.css"
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <Navbar.Brand>
-                    <img src={logo} alt="logo" height="45" />
-                </Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt="logo" height="45" />
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto text-left">
-                        <Nav.Link><i className="fas fa-home"></i> Home</Nav.Link>
-                        <Nav.Link><i className="fas fa-sign-in-alt"></i> Sign in</Nav.Link>
-                        <Nav.Link><i className="fas fa-user-plus"></i> Sign up</Nav.Link>
+                        <NavLink to="/" className={styles.NavLink}><i className="fas fa-home"></i> Home</NavLink>
+                        <NavLink to="/signin" className={styles.NavLink}><i className="fas fa-sign-in-alt"></i> Sign in</NavLink>
+                        <NavLink to="/signup" className={styles.NavLink}><i className="fas fa-user-plus"></i> Sign up</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
